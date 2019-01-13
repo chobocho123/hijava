9b56201d4ef2107ee4b3a3412f53c6442f8b7874
chobocho123 <ijn10000@naver.com>
2019-01-13 13:49:25 +0900

    실습

---
+++ src/hijava/prac/IntFloat4.java
@@ -0,0 +1,20 @@
+package hijava.prac;
+
+public class IntFloat4 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		double d = 2.0;
+		int	r1 = (int)(i+d);
+		int	r2 = (int)(i-d);
+		int	r3 = (int)(i*d);
+		int	r4 = (int)(i/d);
+		float r5=(float)12.4%3;
+		System.out.println(r1);
+		System.out.println(r2);
+		System.out.println(r3);
+		System.out.println(r4);
+		System.out.println(r5);
+	}
+
+}
