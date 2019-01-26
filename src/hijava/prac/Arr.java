6687c6778aebd72e0fc43eea40cc097889b6dbe1
chobocho123 <ijn10000@naver.com>
2019-01-26 12:46:21 +0900

    java

---
+++ src/hijava/prac/Arr.java
@@ -0,0 +1,49 @@
+package hijava.prac;
+
+public class Arr {
+
+	public static void main(String[] args) {
+		ex1();
+		ex2();
+		ex3();
+	
+	}
+
+	private static void ex3() {
+		String[] strk = new String[10];
+		int len = strk.length;
+		for(int i = 0 ; i < len; i++) {
+			strk[i] = "김" + (i + 1) + "수";
+		}
+		for(int i = 0 ; i < len; i++) {
+			System.out.println(strk[i]);
+		}
+
+		
+	}
+
+	private static void ex2() {
+		String[] strs = new String[100];
+		int len = strs.length;
+		for(int i = 0; i < len; i++) {
+			strs[i] = "str" + (i + 1);
+		}
+		for(int i = 0; i < len; i++) {
+			System.out.println(strs[i]);
+		}
+	}
+
+	private static void ex1() {
+		int[] nums = new int[100];
+		
+		for( int i = 0; i < nums.length; i++) {
+			nums[i] = i + 1;
+		}
+		for( int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+	}
+	
+	
+
+}
